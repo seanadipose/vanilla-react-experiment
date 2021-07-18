9043cd06351a520f7b8d5f9a255beea3a4c30406
Sean Hamilton <me@seanhamilton.dev>
2021-07-18 10:50:07 -0600

    added hooks for animal & breed and inputs

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,14 @@
 import { useState } from 'react';
 
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
+
 const SearchParams = () => {
+  /* The order of the hooks is really important. It will get applied to the order the variables are declared
+   */
   const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('dog');
+  const [breed, setBreed] = useState('');
+  const breeds = [];
 
   /* target.value is the result of the event */
   const updateLocation = function (e) {
@@ -19,6 +26,37 @@ const SearchParams = () => {
           placeholder="Location"
           onChange={(e) => updateLocation(e)}
         />
+        <label htmlFor="animal"> Animal</label>
+        <select
+          name="animal"
+          id="animal"
+          value={animal}
+          onChange={(e) => setAnimal(e.target.value)}
+          onBlur={(e) => setAnimal(e.target.value)}
+        >
+          <option />
+          {ANIMALS.map((animal) => (
+            <option value={animal} key={animal}>
+              {animal}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed</label>
+
+        <select
+          name="breed"
+          id="breed"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+          onBlur={(e) => setBreed(e.target.value)}
+        >
+          <option />
+          {breeds.map((breed) => (
+            <option value={breed} key={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
         <button>Submit</button>
       </form>
     </div>
